3fba0ced791e9c618d89a22da535f1ce79604640
root <noahignaciosmith@gmail.com>
2017-12-06 02:56:01 +0000

    Added html templates for index and profiles started implementing spotify oauth

--- configs/spotify-music-maps.wsgi
+++ configs/spotify-music-maps.wsgi
@@ -5,4 +5,4 @@ logging.basicConfig(stream=sys.stderr)
 sys.path.insert(0,"/var/www/spotify-music-maps/")
 
 from run import app as application
-application.secret_key = '12345'
+application.secret_key = '\xe2\x1dy\xcc\xca\xdasl(\xc3KS\xd1c\xf5\xb8\xcdQ\x90wsF\x8a{'
--- run.py
+++ run.py
@@ -1,17 +1,68 @@
-from flask import Flask
+import json
+from flask import Flask, request, redirect, g, render_template
 from flask_sqlalchemy import SQLAlchemy
+import requests
+import base64
+import urllib
+from urlparse import urlparse
 
 app = Flask(__name__)
+
 app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://noahismith:spotify-mmaps@spotify-mmaps.cnfthxsaxcoe.us-east-2.rds.amazonaws.com/spotifymmaps'
 app.config['SQLALCHEMY_ECHO'] = True
 app.config['DEBUG'] = True
+
 db = SQLAlchemy(app)
+
+#  Client Keys
+CLIENT_ID = "113d03ef95bd4e29889577312ec0817d"
+CLIENT_SECRET = "4fd53ac1a0554655b3b9f12d3c2c8421"
+
+# Spotify URLS
+SPOTIFY_AUTH_URL = "https://accounts.spotify.com/authorize"
+SPOTIFY_TOKEN_URL = "https://accounts.spotify.com/api/token"
+SPOTIFY_API_BASE_URL = "https://api.spotify.com"
+API_VERSION = "v1"
+SPOTIFY_API_URL = "{}/{}".format(SPOTIFY_API_BASE_URL, API_VERSION)
+
+
+# Server-side Parameters
+CLIENT_SIDE_URL = "http://52.15.141.175"
+REDIRECT_URI = CLIENT_SIDE_URL + "/profile"
+SCOPE = "playlist-modify-public playlist-modify-private"
+STATE = ""
+SHOW_DIALOG_bool = True
+SHOW_DIALOG_str = str(SHOW_DIALOG_bool).lower()
+
+
+auth_query_parameters = {
+    "response_type": "code",
+    "redirect_uri": REDIRECT_URI,
+    "scope": SCOPE,
+    # "state": STATE,
+    # "show_dialog": SHOW_DIALOG_str,
+    "client_id": CLIENT_ID
+}
+
+@app.route("/")
+def index():
+	return render_template('index.html') 	
 
 from models import User
 
-@app.route("/")
-def hello():
-    return "Welcome to Spotify Music Maps!"
+@app.route("/login")
+def login():
+	# Auth Step 1: Authorization
+	url_args = "&".join(["{}={}".format(key,urllib.quote(val)) for key,val in auth_query_parameters.iteritems()])
+	auth_url = "{}/?{}".format(SPOTIFY_AUTH_URL, url_args)
+	return redirect(auth_url)
+
+@app.route("/profile")
+def profile():
+	if ("error" in request.query_string):
+		return render_template('index.html')
+	else: 
+		return render_template('profile.html')
 
 @app.route("/test")
 def create_user():
